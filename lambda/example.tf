fcf1b99a123b04819de3dc84226d539a7ba82fe4
mashiike <ikeda-masashi@kayac.com>
2023-09-07 16:21:19 +0900

    add lambda sample

---
+++ lambda/example.tf
@@ -0,0 +1,133 @@
+
+resource "aws_iam_role" "canyon_example" {
+  name = "canyon-example"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "canyon_example" {
+  name   = "canyon-example"
+  path   = "/"
+  policy = data.aws_iam_policy_document.canyon_example.json
+}
+
+resource "aws_iam_role_policy_attachment" "canyon_example" {
+  role       = aws_iam_role.canyon_example.name
+  policy_arn = aws_iam_policy.canyon_example.arn
+}
+
+resource "aws_sqs_queue" "canyon_example" {
+  name                       = "canyon-example"
+  message_retention_seconds  = 86400
+  visibility_timeout_seconds = 30
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.canyon_example_dlq.arn
+    maxReceiveCount     = 3
+  })
+}
+
+resource "aws_sqs_queue" "canyon_example_dlq" {
+  name                      = "canyon-example-dlq"
+  message_retention_seconds = 345600
+}
+
+data "aws_iam_policy_document" "canyon_example" {
+  statement {
+    resources = [
+      aws_sqs_queue.canyon_example.arn,
+    ]
+    actions = [
+      "sqs:DeleteMessage",
+      "sqs:GetQueueUrl",
+      "sqs:ChangeMessageVisibility",
+      "sqs:ReceiveMessage",
+      "sqs:SendMessage",
+      "sqs:GetQueueAttributes",
+    ]
+  }
+  statement {
+    actions = [
+      "logs:GetLog*",
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+    resources = ["*"]
+  }
+}
+
+data "archive_file" "canyon_example_dummy" {
+  type        = "zip"
+  output_path = "${path.module}/canyon_example_dummy.zip"
+  source {
+    content  = "canyon_example_dummy"
+    filename = "bootstrap"
+  }
+  depends_on = [
+    null_resource.canyon_example_dummy
+  ]
+}
+
+resource "null_resource" "canyon_example_dummy" {}
+
+resource "aws_lambda_function" "canyon_example" {
+  lifecycle {
+    ignore_changes = all
+  }
+
+  function_name = "canyon-example"
+  role          = aws_iam_role.canyon_example.arn
+
+  handler  = "bootstrap"
+  runtime  = "provided.al2"
+  filename = data.archive_file.canyon_example_dummy.output_path
+}
+
+resource "aws_lambda_alias" "canyon_example" {
+  lifecycle {
+    ignore_changes = all
+  }
+  name             = "current"
+  function_name    = aws_lambda_function.canyon_example.arn
+  function_version = aws_lambda_function.canyon_example.version
+}
+
+
+resource "aws_lambda_function_url" "canyon_example" {
+  function_name      = aws_lambda_alias.canyon_example.function_name
+  qualifier          = aws_lambda_alias.canyon_example.name
+  authorization_type = "NONE"
+
+  cors {
+    allow_credentials = true
+    allow_origins     = ["*"]
+    allow_methods     = ["GET", "POST"]
+    expose_headers    = ["keep-alive", "date"]
+    max_age           = 0
+  }
+}
+
+resource "aws_lambda_event_source_mapping" "canyon_example" {
+  batch_size       = 10
+  event_source_arn = aws_sqs_queue.canyon_example.arn
+  enabled          = true
+  function_name    = aws_lambda_alias.canyon_example.arn
+}
+
+
+output "lambda_function_url" {
+  description = "Generated function URL"
+  value       = aws_lambda_function_url.canyon_example.function_url
+}
---
+++ lambda/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"io"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/mashiike/canyon"
+)
+
+func main() {
+	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{Level: slog.LevelDebug})))
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
+	defer cancel()
+
+	opts := []canyon.Option{
+		canyon.WithServerAddress(":8080", "/"),
+	}
+	err := canyon.RunWithContext(ctx, "canyon-example", http.HandlerFunc(handler), opts...)
+	if err != nil {
+		slog.Error("failed to run canyon", "error", err)
+		os.Exit(1)
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	logger := canyon.Logger(r)
+	if !canyon.IsWorker(r) {
+		logger.Info("server process", slog.String("request", r.URL.Path))
+		// handle webhook directly
+		messageId, err := canyon.SendToSQS(r, nil)
+		if err != nil {
+			logger.Error("failed to send sqs message", "error", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		logger.Info("send sqs message", slog.String("message_id", messageId), slog.String("method", r.Method), slog.String("path", r.URL.Path))
+		w.WriteHeader(http.StatusAccepted)
+		return
+	}
+
+	// handle from sqs message
+	logger.Info("worker process", slog.String("request", r.URL.Path))
+	bs, err := io.ReadAll(r.Body)
+	if err != nil {
+		logger.Error("failed to read body", "error", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	logger.Info("event request body", slog.String("body", string(bs)))
+	w.WriteHeader(http.StatusOK) // if 2xx is success, sqs message will be deleted
+}
